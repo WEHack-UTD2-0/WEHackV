2920c74fd4cbcddfa2ca767b2bd58eaf21b8dad4
Sneha <86683361+snehab2@users.noreply.github.com>
2024-12-02 17:25:22 -0600

    updated font space

--- apps/web/src/components/registration/CreatingRegistration.tsx
+++ apps/web/src/components/registration/CreatingRegistration.tsx
@@ -4,7 +4,7 @@ export default function CreatingRegistration() {
 	return (
 		<main className="absolute bottom-0 left-0 z-10 flex min-h-screen w-screen flex-col items-center justify-center gap-y-12 bg-background">
 			<h1 className="text-3xl font-black">Creating Your Registration!</h1>
-			<h2 className="text-2xl font-black">Please don’t refresh or exit the page!</h2>
+			<h2 className="text-2xl font-black tracking-wide">Please don’t refresh or exit the page!</h2>
 			<Bars
 				height="80"
 				width="80"
